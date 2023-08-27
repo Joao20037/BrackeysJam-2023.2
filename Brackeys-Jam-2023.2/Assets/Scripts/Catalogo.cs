009c9a2ba04939dc6a901c7dc13d5d569f5f47f4
Lucas-FAlves <Lucas.3451@hotmail.com>
2023-08-27 01:06:41 -0300

    a

--- Brackeys-Jam-2023.2/Assets/Scripts/Catalogo.cs
+++ Brackeys-Jam-2023.2/Assets/Scripts/Catalogo.cs
@@ -19,7 +19,14 @@ public class Catalogo : MonoBehaviour
     
     public void Efeito(int pontos)
     {
-        
+        if(pontos == 3 || pontos == 6 || pontos == 9)
+        {
+            GameObject.FindGameObjectWithTag("Submarino").GetComponent<MovementSubmarine>().UpdateDeepLimit(10*(pontos/3));
+        }
+        else if (pontos == 2 || pontos == 4 || pontos == 8 )
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<MovementPlayer>().UpdatePlayerSpeed(2*(pontos/2));
+        }
     }
     
     
