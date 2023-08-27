a43d3c5febfeeea05b6f1f4e176c34c937b9f70e
Joao20037 <joaolucas.moraes.oliveira@gmail.com>
2023-08-26 22:23:09 -0300

    Animation Submarine and Player

--- Brackeys-Jam-2023.2/Assets/Scripts/MovementPlayer.cs
+++ Brackeys-Jam-2023.2/Assets/Scripts/MovementPlayer.cs
@@ -9,6 +9,8 @@ public class MovementPlayer : MonoBehaviour
     [SerializeField] private float jumpStrength;
     private Rigidbody2D rb;
     private InputControls inputs;
+    private float x_scale;
+    [SerializeField] Animator animator;
 
     private void OnEnable()
     {
@@ -24,6 +26,7 @@ public class MovementPlayer : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         inputs = new InputControls();
+        x_scale = transform.localScale.x;
     }
 
     // Update is called once per frame
@@ -31,11 +34,29 @@ public class MovementPlayer : MonoBehaviour
     {
         Move(inputs.PlayerControlMovement.Move.ReadValue<Vector2>());
         Jump(inputs.PlayerControlMovement.Jump.ReadValue<float>());
+        animator.SetFloat("VelocityY", rb.velocity.y);
     }
 
     private void Move(Vector2 movementDirection)
     {
         rb.velocity = new Vector3(movementDirection.x,movementDirection.y, 0) * speed;
+
+        if (rb.velocity.x > 0)
+        {
+            animator.SetBool("isMoving", true);
+            if (transform.localScale.x < 0) x_scale = -transform.localScale.x;
+            transform.localScale = new Vector3(x_scale, transform.localScale.y, transform.localScale.z);
+        }
+        else if(rb.velocity.x < 0)
+        {
+            animator.SetBool("isMoving", true);
+            if (transform.localScale.x > 0) x_scale = -transform.localScale.x;
+            transform.localScale = new Vector3(x_scale, transform.localScale.y, transform.localScale.z);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
     }
 
     private void Jump(float action)
@@ -43,6 +64,11 @@ public class MovementPlayer : MonoBehaviour
         if (action == 1)
         {
             rb.AddForce(new Vector2(0, 1) * jumpStrength);
+            animator.SetBool("Space", true);
+        }
+        else
+        {
+            animator.SetBool("Space", false);
         }
     }
 
