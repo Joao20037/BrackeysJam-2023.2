54fb52223da2bc7db9c8e9636b8c9d4f63387ac3
pedroaugustosgs <37881747+pedroaugustosgs@users.noreply.github.com>
2023-08-27 04:24:31 -0300

    por favor me deixa dormir

--- Brackeys-Jam-2023.2/Assets/Scripts/Catalogo.cs
+++ Brackeys-Jam-2023.2/Assets/Scripts/Catalogo.cs
@@ -21,11 +21,11 @@ public class Catalogo : MonoBehaviour
     {
         if(pontos == 3 || pontos == 6 || pontos == 9)
         {
-            GameObject.FindGameObjectWithTag("Submarino").GetComponent<MovementSubmarine>().UpdateDeepLimit(10*(pontos/3));
+            //GameObject.FindGameObjectWithTag("Submarino").GetComponent<MovementSubmarine>().UpdateDeepLimit(10*(pontos/3));
         }
         else if (pontos == 2 || pontos == 4 || pontos == 8 )
         {
-            GameObject.FindGameObjectWithTag("Player").GetComponent<MovementPlayer>().UpdatePlayerSpeed(2*(pontos/2));
+            //GameObject.FindGameObjectWithTag("Player").GetComponent<MovementPlayer>().UpdatePlayerSpeed(2*(pontos/2));
         }
     }
     
