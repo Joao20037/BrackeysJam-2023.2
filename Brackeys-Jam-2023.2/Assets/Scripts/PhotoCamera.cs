8455bba9e41d5952efb456b4a785f7801050b0e2
Joao20037 <joaolucas.moraes.oliveira@gmail.com>
2023-09-03 17:04:23 -0300

    eita

--- Brackeys-Jam-2023.2/Assets/Scripts/PhotoCamera.cs
+++ Brackeys-Jam-2023.2/Assets/Scripts/PhotoCamera.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PhotoCamera : MonoBehaviour
-{   
+{
     FOV fov;
     GameObject peixe;
     // Start is called before the first frame update
@@ -15,14 +15,13 @@ public class PhotoCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetMouseButtonDown(0) && fov.targetsInRadius.Length > 0)
-       {
-                AudioManager.instance.PlaySound("camera");
-                Shoot(fov.targetsInRadius);
-       } 
+        if (Input.GetMouseButtonDown(0) && fov.targetsInRadius.Length > 0)
+        {
+            Shoot(fov.targetsInRadius);
+        }
     }
     private IEnumerator Atordoar(GameObject peixe)
-    {   
+    {
         //peixe.GetComponent<FishMoviment>().enabled = false;
         peixe.GetComponent<FishHunt>().enabled = false;
         yield return new WaitForSeconds(3f);
@@ -30,10 +29,18 @@ public class PhotoCamera : MonoBehaviour
         peixe.GetComponent<FishHunt>().enabled = true;
     }
 
+    private IEnumerator Flash()
+    {
+        Color _colorBlue = gameObject.GetComponent<MeshRenderer>().materials[0].color;
+        Color _colorWhite = gameObject.GetComponent<MeshRenderer>().materials[1].color;
+        gameObject.GetComponent<MeshRenderer>().materials[0].color = _colorWhite;
+        yield return new WaitForSeconds(1f);
+        gameObject.GetComponent<MeshRenderer>().materials[0].color = _colorBlue;
+    }
+
+
     public void Shoot(Collider2D[] peixes)
     {
-        
-
         float dist = 0;
         Debug.Log(peixes.Length);
         foreach (Collider2D mano in peixes)
@@ -45,20 +52,24 @@ public class PhotoCamera : MonoBehaviour
                 Debug.Log(peixe.name);
             }
         }
-        string tag = peixe.tag;
         //Atordoa Peixe
         StartCoroutine(Atordoar(peixe));
-        
+        StartCoroutine(Flash());
+
         //Atualiza catálogo
-        Catalogo catalogo = GameObject.FindGameObjectWithTag("Player").GetComponent<Catalogo>();
-        if(catalogo.registros.TryGetValue(tag, out int value))
-        {   
-            value++;
-            if (value == catalogo.Checkpoints[tag])
-            {   
-                catalogo.registros[tag] = value;
-                catalogo.upgradePoints++;
-                catalogo.Efeito(catalogo.upgradePoints);
+        string tag = peixe.tag;
+        Catalogo catalogo = GameObject.FindGameObjectWithTag("Catalogo").GetComponent<Catalogo>();
+        foreach (DadosPeixe registro in catalogo.peixes)
+        {
+            if (registro.nomePeixe == tag)
+            {
+                registro.fotografados++;
+                if (registro.fotografados == registro.meta)
+                {
+                    catalogo.upgradePoints++;
+                    catalogo.Efeito(catalogo.upgradePoints);
+                }
+                return;
             }
         }
     }
